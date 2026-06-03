de00a2aaf0e677c8605107c35a36e9dc68c4afb4
BonganiChauke <bonganichauke11@outlook.com>
2026-06-03 22:04:17 +0200

    Style reference

--- app/pages/404.php
+++ app/pages/404.php
@@ -10,6 +10,9 @@
     <!-- Bootstrap CSS v5.3.8 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous" />
+
+    <!-- custom css style file link -->
+    <link rel="stylesheet" href="./assets/css/style.css?344">
 </head>
 
 <body>
