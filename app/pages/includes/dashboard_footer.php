f5011f5b038c2e93157afa38f10e64db7f05f5ae
BonganiChauke <bonganichauke11@outlook.com>
2026-08-20 18:01:51 +0200

    head and footer page for dashboard pages

---
+++ app/pages/includes/dashboard_footer.php
@@ -0,0 +1,17 @@
+</main>
+</div><!-- /shell -->
+
+<footer class="dash-footer" id="siteFooter">
+    <p>&copy; <?= date('Y') ?> <span>Snap Gvng Ent</span>. All rights reserved. <?= $pageFooter ?> Panel. </p>
+</footer>
+
+<!-- Bootstrap JavaScript Bundle (includes Popper) -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+    crossorigin="anonymous"></script>
+
+<!-- custom js  -->
+<script src="<?= ROOT ?>/assets/js/script.js" defer></script>
+</body>
+
+</html>
