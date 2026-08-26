a2ae8929bf4bae731f7767ec9382c365d018bd8f
BonganiChauke <bonganichauke11@outlook.com>
2026-08-26 22:07:58 +0200

    removed closing body tag

--- app/pages/includes/head.php
+++ app/pages/includes/head.php
@@ -7,4 +7,4 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="<?= BASE_URL ?>css/style.css?344">
 </head>
-<body></body>
+<body>
